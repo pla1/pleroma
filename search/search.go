ac738d5bb3c59428987ba2e2982c3343cff24723
Patrick L Archibald (PLA) <patrick.archibald@gmail.com>
2019-01-21 21:46:28 -0500

    Search tweaks.

--- search/search.go
+++ search/search.go
@@ -10,20 +10,21 @@ import (
 
 func main() {
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=require host=%s",
-		os.Getenv("PLEROMA_DB_USER"), os.Getenv("PLEROMA_DB_PASSWORD"), os.Getenv("PLEROMA_DB_NAME"), os.Getenv("PLEROMA_DB_HOST"))
+		os.Getenv("PLEROMA_DB_USER"),
+		os.Getenv("PLEROMA_DB_PASSWORD"),
+		os.Getenv("PLEROMA_DB_NAME"),
+		os.Getenv("PLEROMA_DB_HOST"))
 	db, err := sql.Open("postgres", dbinfo)
 	checkErr(err)
 	defer db.Close()
 	var searchString = os.Args[1]
 	fmt.Printf("# Querying activities for: %v\n", searchString)
-	rows, err := db.Query(`SELECT id, data -> 'object' ->> 'url' as url 
-                       FROM activities 
-                       where data ->> 'type' = 'Create' 
-                       and data -> 'object' ->> 'type' = 'Note' 
-                       and data -> 'object' ->> 'url' is not null 
+	rows, err := db.Query(`SELECT id, data -> 'object' ->> 'url' as url
+                       FROM activities
+                       where data ->> 'type' = 'Create'
+                       and data -> 'object' ->> 'type' = 'Note'
+                       and data -> 'object' ->> 'url' is not null
                        and data -> 'object' ->> 'content' like $1
-                       order by id desc
-                       fetch first 100 rows only
                        `, searchString)
 	checkErr(err)
 	var id string
